310f2cdf38d7bc63923be384c6ff30c02c819a9a
xxsslt <108867640+xxsslt@users.noreply.github.com>
2022-07-07 17:44:56 +0800

    Add files via upload

---
+++ Web-projects/test-site/scripts/main.js
@@ -0,0 +1,28 @@
+
+//let myHeading = document.querySelector('h1');替换标题
+//myHeading.textContent = 'Hello world!';
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/1.jpg') {
+      myImage.setAttribute('src', 'images/2.jpg');
+    } else {
+      myImage.setAttribute('src', 'images/1.jpg');
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+  let myName = prompt('请输入你的名字。');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = '莉莉丝，' + myName;
+  }
+}
+myButton.onclick = function() {
+   setUserName();
+}
